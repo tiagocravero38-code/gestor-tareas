77b1d8718d1de90fe7587462c1d96c98ae53b1d3
Tiago Cravero <402454@sistemas.frc.utn.edu.ar>
2026-09-11 19:11:46 -0300

    app.js cargado

---
+++ app.js
@@ -0,0 +1,15 @@
+// ============================================
+//  GESTOR DE TAREAS — DDS
+// ============================================
+
+console.log('app.js cargado');
+
+// ---- Referencias a los elementos del HTML ----
+const formTarea      = document.querySelector('#form-tarea');
+const inputTexto     = document.querySelector('#texto');
+const selectPrioridad = document.querySelector('#prioridad');
+const pError         = document.querySelector('#error');
+const ulLista        = document.querySelector('#lista');
+const pEstadisticas  = document.querySelector('#estadisticas');
+
+console.log(ulLista);
